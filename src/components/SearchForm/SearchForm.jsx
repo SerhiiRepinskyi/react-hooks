3f30b32e212794aa56031d8d35ad5451b78a23b0
Igor <115535399+IgorVinnik85@users.noreply.github.com>
2023-05-21 16:30:10 +0300

    update_hooks

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,32 +1,30 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 
 import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
-export class SearchForm extends Component {
-state = {
-  searchQuery: ''
-}
+export const SearchForm = ({ onSubmit }) => {
+  const [searchQuery, setSearchQuery] = useState('');
 
-handleFormSubmit = (e) => {
-e.preventDefault();
-this.props.onSubmit(this.state.searchQuery)
-this.setState({searchQuery: ''})
-}
-  render() {
-    
-    return <SearchFormStyled onSubmit={this.handleFormSubmit}>
-    <FormBtn type="submit">
-      <FiSearch size="16px" />
-    </FormBtn>
-    <InputSearch
-      placeholder="What do you want to write?"
-      name="search"
-      required
-      autoFocus
-      value={this.state.searchQuery}
-      onChange={(e) => this.setState({searchQuery: e.target.value})}
-    />
-  </SearchFormStyled>;
-  }
-}
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    onSubmit(searchQuery);
+    setSearchQuery('');
+  };
+
+  return (
+    <SearchFormStyled onSubmit={handleFormSubmit}>
+      <FormBtn type="submit">
+        <FiSearch size="16px" />
+      </FormBtn>
+      <InputSearch
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        autoFocus
+        value={searchQuery}
+        onChange={e => setSearchQuery(e.target.value)}
+      />
+    </SearchFormStyled>
+  );
+};
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -1,64 +1,56 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
-export class Gallery extends Component {
-  state = {
-    search: '',
-    page: 1,
-    photos: [],
-    isVisible: false,
+export const Gallery = () => {
+  const [search, setSearch] = useState('');
+  const [page, setPage] = useState(1);
+  const [photos, setPhotos] = useState([]);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const hendleSubmit = query => {
+    setSearch(query);
+    setPhotos([]);
+    setPage(1);
   };
 
-  hendleSubmit = query => {
-    this.setState({ search: query, photos: [], page: 1 });
-  };
-
-  componentDidUpdate(props, prevState) {
-    if (this.state.search === '') {
-      return <h1>Sorry, type some text in search form</h1>;
+  useEffect(() => {
+    if (search === '') {
+      return 
     }
-    if (
-      prevState.search !== this.state.search ||
-      prevState.page !== this.state.page
-    ) {
-      ImageService.getImages(this.state.search, this.state.page).then(
-        response =>
-          this.setState(prevState => ({
-            photos: [...prevState.photos, ...response.data.photos],
-            isVisible:
-              response.data.page <
-              Math.ceil(response.data.total_results / response.data.per_page),
-          }))
+
+    ImageService.getImages(search, page).then(response => {
+      setPhotos(prevState => [...prevState, ...response.data.photos]);
+      setIsVisible(
+        response.data.page <
+          Math.ceil(response.data.total_results / response.data.per_page)
       );
-    }
-  }
-  loadMoreBtn = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-  render() {
-    return (
-      <>
-        <SearchForm onSubmit={this.hendleSubmit} />
-        <Grid>
-          {this.state.photos.length > 0 &&
-            this.state.photos.map(({ id, avg_color, src, alt }) => (
-              <GridItem key={id}>
-                <CardItem color={avg_color}>
-                  <img src={src.large} alt={alt} />
-                </CardItem>
-              </GridItem>
-            ))}
-        </Grid>
+    });
+  }, [search, page]);
 
-        {!this.state.photos.length && (
-          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
-        )}
-        {this.state.isVisible && <Button onClick={this.loadMoreBtn}>Load More</Button>}
-      </>
-    );
-  }
-}
+  const loadMoreBtn = () => {
+    setPage(prev => prev + 1);
+  };
+
+  return (
+    <>
+      <SearchForm onSubmit={hendleSubmit} />
+      <Grid>
+        {photos.length > 0 &&
+          photos.map(({ id, avg_color, src, alt }) => (
+            <GridItem key={id}>
+              <CardItem color={avg_color}>
+                <img src={src.large} alt={alt} />
+              </CardItem>
+            </GridItem>
+          ))}
+      </Grid>
+
+      {!photos.length && (
+        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+      )}
+      {isVisible && <Button onClick={loadMoreBtn}>Load More</Button>}
+    </>
+  );
+};
