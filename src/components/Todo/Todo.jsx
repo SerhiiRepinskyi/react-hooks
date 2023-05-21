965d4df50c2e688ed50e84f9e04083046f5e7e5c
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-05-21 16:10:17 +0300

    task 2 completed

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,7 +1,34 @@
+import { useState } from 'react';
 import { Text } from 'components';
 import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
 import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 
-export const Todo = () => {
-  return <h2>Todo</h2>;
+export const Todo = ({ text, id, handleDelete, index, handleEdit }) => {
+  const [newText, setNewText] = useState(text);
+  const [isEdit, setIsEdit] = useState(false);
+
+  return (
+    <TodoWrapper>
+      <Text textAlign="center" marginBottom="20px">
+        TODO {index}
+      </Text>
+
+      {isEdit ? (
+        <input
+          onBlur={() => {
+            setIsEdit(false);
+            handleEdit({ id, text: newText });
+          }}
+          autoFocus
+          value={newText}
+          onChange={e => setNewText(e.target.value)}
+        />
+      ) : (
+        <Text onClick={() => setIsEdit(true)}>{text}</Text>
+      )}
+      <DeleteButton type="button" onClick={() => handleDelete(id)}>
+        <RiDeleteBinLine size={24} />
+      </DeleteButton>
+    </TodoWrapper>
+  );
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,52 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  };
+
+  handleSubmit = string => {
+    this.setState(prev => ({
+      todos: [...prev.todos, { id: nanoid(), text: string }],
+    }));
+  };
+
+  handleDelete = id => {
+    this.setState(prev => ({
+      todos: prev.todos.filter(el => el.id !== id),
+    }));
+  };
+
+  handleEdit = todo => {
+    this.setState(prev => ({
+      todos: prev.todos.map(el => {
+        if (el.id === todo.id) {
+          el.text = todo.text;
+        }
+        return el;
+      }),
+    }));
+  };
+
   render() {
-    return <Text>Todos</Text>;
+    return (
+      <>
+        <Text>Todos</Text>
+        <SearchForm onSubmit={this.handleSubmit} />
+        <Grid>
+          {this.state.todos.map((el, index) => (
+            <GridItem key={el.id}>
+              <Todo
+                text={el.text}
+                id={el.id}
+                handleDelete={this.handleDelete}
+                index={index + 1}
+                handleEdit={this.handleEdit}
+              />
+            </GridItem>
+          ))}
+        </Grid>
+      </>
+    );
   }
 }
